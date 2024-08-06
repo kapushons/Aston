9242b445cb10f9d86126055a6c5bf75c14683224
kapushons <sergej.sorokin.2014@mail.ru>
2024-08-06 04:13:16 +0300

    Решены задания 1-13

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        // Первое задание //
+        // 1 задание //
         Lesson2.printThreeWords();
-        // Второе задание //
+        // 2 задание //
         Lesson2.checkSumSign();
-        // Третье задание //
+        // 3 задание //
         Lesson2.printColor();
-        // Четвёртое задание //
+        // 4 задание //
         Lesson2.compareNumbers();
-        // Пятое задание //
+        // 5 задание //
+        Lesson2.numberLine();
+        // 6 задание //
+        Lesson2.numberValue();
+        // 7 задание //
+        Lesson2.returnNumber();
+        // 8 задание //
+        Lesson2.stringAndNumber();
+        // 9 задание //
+        Lesson2.years();
+        // 10 задание//
+        Lesson2.array();
+        // 11 задание //
+        Lesson2.arrayHundred();
+        // 12 задание //
+        Lesson2.arrayMultiplication();
+        // 13 задание //
+        Lesson2.arraySquare();
+        // 14 задание //
         }
     }
