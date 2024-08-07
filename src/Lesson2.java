56c846644a4425f55408cadb4081a5ed8e8c9206
kapushons <sergej.sorokin.2014@mail.ru>
2024-08-07 13:26:36 +0300

    сдаю 14 заданий

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -104,6 +104,7 @@ public class Lesson2 {
         for (int i = 0; i < 7; i++)
             System.out.println(Arrays.toString(arr[i]));
     }
+
     public static int[] arrayLen(int len, int initialValue) {
         int[] arr = new int[len];
         for (int i = 0; i < len; i++) {
